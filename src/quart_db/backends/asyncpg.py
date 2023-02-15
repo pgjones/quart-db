6a89ba02395c61978817383832940f3142ccc108
Devon Stewart <blast@hardchee.se>
2023-02-15 15:29:26 -0500

    Avoid double-close if the pool has already been disconnected

--- src/quart_db/backends/asyncpg.py
+++ src/quart_db/backends/asyncpg.py
@@ -159,7 +159,8 @@ class Backend(BackendABC):
             self._pool = await asyncpg.create_pool(dsn=self._url, init=self._init)
 
     async def disconnect(self, timeout: Optional[int] = None) -> None:
-        await asyncio.wait_for(self._pool.close(), timeout)
+        if self._pool is not None:
+            await asyncio.wait_for(self._pool.close(), timeout)
         self._pool = None
 
     async def acquire(self) -> Connection:
